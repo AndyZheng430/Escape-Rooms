52299832dbe50a4e20afd8a923c55202639e8baa
AndyZheng430 <zhenga6@bxscience.edu>
2020-11-19 18:18:48 -0500

    added lock

---
+++ Escape/Assets/Scripts/LockController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockController : MonoBehaviour
+{
+    private int[] result, correctCombo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        result = new int[] {0, 0, 0};
+        correctCombo = new int[] {3, 4, 5};
+        Rotate.Rotated += CheckResults;
+    }
+
+    void CheckResults(string wheelName, int number) 
+    {
+        switch (wheelName)
+        {
+            case "wheel1":
+                result[0] = number;
+                break;
+            case "wheel2":
+                result[1] = number;
+                break;
+            case "wheel3":
+                result[2] = number;
+                break;
+        }
+        if (result[0] == correctCombo[0] && result[1] == correctCombo[1] && result[2] == correctCombo[2])
+        {
+            Debug.Log("Open");
+        }
+    }
+    void OnDestroy() {
+        Rotate.Rotated -= CheckResults;    
+    }
+}
---
+++ Escape/Assets/Scripts/Rotate.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Rotate : MonoBehaviour
+{
+    public static event Action<string, int> Rotated = delegate { };
+    private bool coroutineAllowed;
+    private int numbershown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coroutineAllowed = true;
+        numbershown = 0;
+    }
+
+    private void OnMouseDown() {
+        if (coroutineAllowed) 
+        {
+            StartCoroutine ("RotateWheel");
+        }
+    }
+    private IEnumerator RotateWheel()
+    {
+        coroutineAllowed = false;
+        for (int i = 0; i <= 11; i++) 
+        {
+            transform.Rotate(new Vector3(-3f, 0f, 0f));
+            yield return new WaitForSeconds(0.01f);
+        }
+        coroutineAllowed = true;
+        numbershown += 1;
+        if(numbershown > 9)
+        {
+            numbershown = 0;
+        }
+        Debug.Log(numbershown);
+        Rotated(name, numbershown);
+    }
+}
