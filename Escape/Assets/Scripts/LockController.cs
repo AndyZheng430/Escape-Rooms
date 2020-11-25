0876983fe70c50350fae3ccafcf215d6525d92c1
AndyZheng430 <zhenga6@bxscience.edu>
2020-11-25 01:24:39 -0500

    working on waterfall for the spring test scene

--- Escape/Assets/Scripts/LockController.cs
+++ Escape/Assets/Scripts/LockController.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class LockController : MonoBehaviour
 {
     private int[] result, correctCombo;
+    public GameObject[] locks;
     // Start is called before the first frame update
+    public GameObject door;
     void Start()
     {
         result = new int[] {0, 0, 0};
         correctCombo = new int[] {3, 4, 5};
         Rotate.Rotated += CheckResults;
+        locks[0] = this.transform.GetChild(0).gameObject;
+        locks[1] = this.transform.GetChild(1).gameObject;
+        locks[2] = this.transform.GetChild(2).gameObject;
     }
 
     void CheckResults(string wheelName, int number) 
@@ -30,6 +35,10 @@ public class LockController : MonoBehaviour
         if (result[0] == correctCombo[0] && result[1] == correctCombo[1] && result[2] == correctCombo[2])
         {
             Debug.Log("Open");
+            Destroy(door);
+            for (var i = 0; i < locks.Length; i++) {
+                Destroy(locks[i]);
+            }
         }
     }
     void OnDestroy() {
