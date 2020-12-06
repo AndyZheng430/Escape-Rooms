399b3c846374886e5c417a4169515b2019e778cd
AndyZheng430 <zhenga6@bxscience.edu>
2020-12-06 17:22:39 -0500

    added pop up text when clicking certain objects

---
+++ Escape/Assets/Scripts/OnClick.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClick : MonoBehaviour
+{
+    public GameObject popup;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        popup.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("turn off text");
+            popup.SetActive(false);
+        }
+    }
+    private void OnMouseDown() {
+        if ((this.transform.position - player.transform.position).magnitude <= 10) 
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("Left click");
+                popup.SetActive(true);
+            }
+        }
+    }
+}
