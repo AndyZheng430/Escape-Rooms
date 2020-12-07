880c27139b070b3d7c7d2b3aad262a5871e9be2f
Alex Cuba <alex.cuba@stonybrook.edu>
2020-12-07 14:57:17 -0500

    Timer UI and Game Over Screen

---
+++ Escape/Assets/Scripts/TimerCountdown.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimerCountdown : MonoBehaviour
+{
+    int CountdownStart = 300;
+    public Text UITimer; 
+    // Start is called before the first frame update
+    void Start()
+    {
+       countdownTimer(); 
+    }
+
+    void countdownTimer()
+    {
+        if(CountdownStart > 0)
+        {
+            TimeSpan countdownInMinutes = TimeSpan.FromSeconds(CountdownStart);
+            UITimer.text = "Timer: " + countdownInMinutes.Minutes + ":" + countdownInMinutes.Seconds;
+            CountdownStart--;
+            Invoke("countdownTimer", 1.0f);
+        }
+        else
+        {
+            UITimer.text= "Game Over!";
+            SceneManager.LoadScene(0);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
