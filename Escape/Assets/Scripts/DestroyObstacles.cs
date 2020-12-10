2220ed53afc1bb0fda5fd28e3aaf941633fb6402
Alex Cuba <alex.cuba@stonybrook.edu>
2020-12-10 02:40:19 -0500

    Finished Winter Level

---
+++ Escape/Assets/Scripts/DestroyObstacles.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObstacles : MonoBehaviour
+{
+    
+public Transform handCon;
+public string objName;
+     void OnMouseDown() {
+        if(handCon.transform.Find(objName))
+            {      
+                Destroy(gameObject);
+            }
+        }
+     }
